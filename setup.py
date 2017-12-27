a9e266c171523445ef7d2b075759906e69373688
Yonatan Perry <yonatan.perry@gmail.com>
2017-12-27 15:35:03 +0200

    build: quicklib's own setup now automatically uses the necessary special commands

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # it is NOT an example of how to write a setup.py that USES quicklib.
 # if you're looking for an example, look at `examplelibrary/setup.py`.
 import os
+import sys
 from setuptools import setup, find_packages
 
 cmdclass = {}
@@ -16,14 +17,21 @@ else:
     import quicklib
     version = quicklib.read_module_version(version_module_path)
 
+script_args = sys.argv[1:]
+if is_packaging:
+    script_args = (
+        ["clean_egg_info", "version_set_by_git", "create_incorporated_zip"] +
+        script_args +
+        ["version_reset_to_dev"]
+    )
+
 setup(
-    cmdclass=cmdclass,
     name='quicklib',
     version=version,
     url="https://github.com/yonatanp/quicklib",
     author='Yonatan Perry',
     author_email='yonatan.perry@gmail.com',
-    description='quicklib: a library for helping with setup of python libraries',
+    description='quicklib: hassle-free setup scripts for your python libraries',
     license='MIT',
     install_requires=[],
     tests_require=[],
@@ -41,6 +49,9 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Operating System :: OS Independent',
     ],
+    zip_safe=False,
+    cmdclass=cmdclass,
+    script_args=script_args,
     command_options={
         'version_set_by_git': {'version_module_paths': ('setup.py', version_module_path)},
     }
